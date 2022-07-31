83d5e7ad96994a57621e1e10ad1e3f7a2dae8cf1
electrified007 <63315246+ogunsolu007@users.noreply.github.com>
2022-07-31 17:51:06 +0100

    fixing the porfolio section

--- src/Container/Porfolio/Portfolio.jsx
+++ src/Container/Porfolio/Portfolio.jsx
@@ -1,34 +1,39 @@
-import React from 'react'
-import "./portfolio.css"
-import fylo from "../../assests/fylo.png"
-import urlshort from "../../assests/urlshortner.png"
-import huddle from "../../assests/huddle.png"
+import React from "react";
+import "./portfolio.css";
+import fylo from "../../assests/fylo.png";
+import urlshort from "../../assests/urlshortner.png";
+import huddle from "../../assests/huddle.png";
 
 const Portfolio = () => {
   return (
     <div className="portfolio">
       <div className="container portfolio-container">
-        <div className="card">
-          <img src={fylo} alt="" />
+        <p className="title">Portfolio</p>
+        <h1 className="subtitle">
+          Some of my
+          <br /> Selected Projects
+        </h1>
+        <div className="cards">
+          <div className="card" data-aos="flip-up">
+            <img src={fylo} alt="" />
+          </div>
+          <div className="card" data-aos="flip-left">
+            <img src={urlshort} alt="" />
+          </div>
+          <div className="card" data-aos="flip-down">
+            <img src={huddle} alt="" />
+          </div>
         </div>
-        <div className="card">
-          <img src={urlshort} alt="" />
-        </div>
-        <div className="card">
-          <img src={huddle} alt="" />
-        </div>
-        <div className="card">
-          <img src={fylo} alt="" />
-        </div>
-        <div className="card">
-          <img src={urlshort} alt="" />
-        </div>
-        <div className="card">
-          <img src={huddle} alt="" />
+        <div className="github">
+          <span></span>
+          <p>
+            For more kindly check my Github Repo{" "}
+            <a href="#" target="_blank">Click Here</a>
+          </p>
         </div>
       </div>
     </div>
   );
-}
+};
 
-export default Portfolio
+export default Portfolio;
